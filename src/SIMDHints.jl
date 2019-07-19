d83e22564b45e7e627723d09f0a52001a81504b7
Takafumi Arakaki <aka.tkf@gmail.com>
2019-07-18 22:15:50 -0700

    Add simd and ivdep hints for broadcasting

--- src/SIMDHints.jl
+++ src/SIMDHints.jl
@@ -1,5 +1,44 @@
 module SIMDHints
 
-greet() = print("Hello World!")
+export simd, ivdep
+
+using Base.Broadcast: Broadcasted, BroadcastStyle, throwdm, preprocess
+
+abstract type SIMDHint end
+
+struct SIMD{T} <: SIMDHint
+    value::T
+end
+
+struct IVDEP{T} <: SIMDHint
+    value::T
+end
+
+simd(x) = SIMD(x)
+ivdep(x) = IVDEP(x)
+
+struct SIMDStyle{f} <: BroadcastStyle end
+
+const HintFuncType = Union{typeof(simd), typeof(ivdep)}
+
+Broadcast.broadcasted(f::HintFuncType, x) =
+    Broadcasted{SIMDStyle{f}}(identity, (x,))
+
+@inline function Base.copyto!(dest::AbstractArray, bc::Broadcasted{SIMDStyle{f}}) where f
+    axes(dest) == axes(bc) || throwdm(axes(dest), axes(bc))
+    @assert bc.f === identity
+    @assert bc.args isa Tuple{Any}
+    bc′ = preprocess(dest, bc.args[1])
+    if f === ivdep
+        @simd ivdep for I in eachindex(bc′)
+            @inbounds dest[I] = bc′[I]
+        end
+    else
+        @simd for I in eachindex(bc′)
+            @inbounds dest[I] = bc′[I]
+        end
+    end
+    return dest
+end
 
 end # module
